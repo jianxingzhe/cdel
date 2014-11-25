412bcb8f1007830f9ef672602861fa666fe92e15
jianxingzhe <liujiwei_DUT@mail.dlut.edu.cn>
2014-11-25 21:09:56 +0800

    a C declaration interpreter
    能够最C语言的复杂声明进行解析

---
+++ cdel.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#define MAXTOKENS 100
+#define MAXTOKENLEN 64
+
+enum type_tag {IDENTIFIER,QUALIFIER,TYPE};
+struct token {
+	char type;
+	char string[MAXTOKENLEN];
+};
+
+int top=-1;
+struct token stack[MAXTOKENS];
+struct token this;
+
+#define pop     stack[top--]
+#define push(s) stack[++top] = s
+
+/*
+ *推断标识符的类型
+ */
+enum type_tag classify_string(void){
+	char *s = this.string;
+	if(!strcmp(s,"const")){
+		strcpy(s,"read-only");
+		return QUALIFIER;
+	}
+	if(!strcmp(s,"volatile")) return QUALIFIER;
+	if(!strcmp(s,"void")) return TYPE;
+	if(!strcmp(s,"char")) return TYPE;
+	if(!strcmp(s,"signed")) return TYPE;
+	if(!strcmp(s,"unsigned")) return TYPE;
+	if(!strcmp(s,"short")) return TYPE;
+	if(!strcmp(s,"int")) return TYPE;
+	if(!strcmp(s,"long")) return TYPE;
+	if(!strcmp(s,"float")) return TYPE;
+	if(!strcmp(s,"double")) return TYPE;
+	if(!strcmp(s,"strcut")) return TYPE;
+	if(!strcmp(s,"union")) return TYPE;
+	if(!strcmp(s,"enum")) return TYPE;
+
+	return IDENTIFIER;
+}
+
+/*
+ *读取下一个标记到this
+ */
+void gettoken(void){
+	char *p = this.string;
+	while((*p = getchar()) == ' ');
+
+	if(isalnum(*p)){
+		while(isalnum(*++p = getchar()));
+		ungetc(*p,stdin);
+		*p = '\0';
+		this.type = classify_string();
+		return;
+	}
+	if(*p == '*'){
+		strcpy(this.string,"pointer to");
+		this.type = '*';
+		return ;
+	}
+	this.string[1] = '\0';
+	this.type = *p;
+	return ;
+}
+
+void read_to_first_identifier(){
+	gettoken();
+	while(this.type != IDENTIFIER){
+		push(this);
+		gettoken();
+	}
+	printf("%s is a ",this.string);
+	gettoken();
+}
+
+void deal_with_arrays(){
+	while(this.type == '['){
+		printf("array ");
+		gettoken();
+		if(isdigit(this.string[0])){
+			printf("0..%d ",atoi(this.string)-1);
+			gettoken();
+		}
+		gettoken();
+		printf("of ");
+	}
+}
+
+void deal_with_function_args(){
+	while(this.type != ')')
+		gettoken();
+	gettoken();
+	printf("function returning ");
+}
+
+void deal_with_pointers(){
+	while(stack[top].type == '*')
+		printf("%s ",pop.string);
+}
+
+void deal_with_declarator(){
+	/*处理标识符之后可能存在的数组/函数*/
+	switch(this.type){
+		case '[': deal_with_arrays();break;
+		case '(': deal_with_function_args();break;
+	}
+
+	deal_with_pointers();
+
+	/*处理在读入到标识符之前压入到堆栈中的符号*/
+	while(top>=0){
+		if(stack[top].type == '('){
+			pop;
+			gettoken();
+			deal_with_declarator();
+		}else
+			printf("%s ",pop.string);
+	}
+}
+
+int main(){
+	read_to_first_identifier();
+	deal_with_declarator();
+	printf("\n");
+	return 0;
+}
+
